a1d51c73a42058d021afc1c41c914d88d28b0e2e
Shane Vorachek <svorach@gmail.com>
2016-05-30 15:08:41 -0400

    Finished implementing WinValidation's getColumn and isVerticalWin methods. Wrote tests to prove getColumn and isVerticalWin are working.

--- src/services/WinValidation.js
+++ src/services/WinValidation.js
@@ -1,4 +1,32 @@
 class WinValidation {
+  checkForWinners() {
+  }
+
+  isHorizontalWin(matrix) {
+    for (let rowIndex = 0; rowIndex < matrix.length; rowIndex ++) {
+      if (this.equalContents(matrix[rowIndex])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  isVerticalWin(matrix) {
+    for (let colIndex = 0; colIndex < matrix[0].length; colIndex ++) {
+      const col = this.getColumn(matrix, colIndex);
+
+      if (this.equalContents(col)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  isDiagonalWin() {
+  }
+
   equalContents(array) {
     const val = array[0];
 
@@ -8,26 +36,19 @@ class WinValidation {
       }
     }
 
+    this.winner = val;
+
     return true;
   }
 
-  checkForWinners() {
-  }
+  getColumn(matrix, columnIndex) {
+    let col = [];
 
-  isHorizontalWin(matrix) {
     for (let row = 0; row < matrix.length; row ++) {
-      if (this.equalContents(matrix[row])) {
-        return true;
-      }
+      col.push(matrix[row][columnIndex]);
     }
 
-    return false;
-  }
-
-  isVerticalWin() {
-  }
-
-  isDiagonalWin() {
+    return col;
   }
 }
 
--- test/services/WinValidation.spec.js
+++ test/services/WinValidation.spec.js
@@ -36,7 +36,39 @@ describe('Win Validation', function() {
     });
   });
 
+  describe('getColumn', function() {
+    it('should be a function', function() {
+      expect(typeof validation.getColumn).to.equal('function');
+    });
+
+    it('should return an array with all of the values in a column of an array matrix', function() {
+      const matrix = [
+        ['x', 'o', 'x'],
+        ['x', 'x', 'o'],
+        ['x', 'x', 'o'],
+      ];
+
+      expect(validation.getColumn(matrix, 0)).to.deep.equal(['x', 'x', 'x']);
+      expect(validation.getColumn(matrix, 1)).to.deep.equal(['o', 'x', 'x']);
+      expect(validation.getColumn(matrix, 2)).to.deep.equal(['x', 'o', 'o']);
+    });
+  });
+
   describe('isHorizontalWin', function() {
+    it('should be a function', function() {
+      expect(typeof validation.isHorizontalWin).to.equal('function');
+    });
+
+    it('should return false if no horizontal win is present', function() {
+      const noHorizontalWin = [
+        ['x', 'o', 'x'],
+        ['x', 'x', 'o'],
+        ['x', 'x', 'o'],
+      ];
+
+      expect(validation.isHorizontalWin(noHorizontalWin)).to.equal(false);
+    });
+
     describe('should return true if a horizontal win is present in', function() {
       it('the first row', function() {
         const firstRow = [
@@ -79,6 +111,67 @@ describe('Win Validation', function() {
         ];
 
         expect(validation.isHorizontalWin(anyRow)).to.equal(true);
+      });
+    });
+  });
+
+  describe('isVerticalWin', function() {
+    it('should be a function', function() {
+      expect(typeof validation.isVerticalWin).to.equal('function');
+    });
+
+    it('should return false if no vertical win is present', function() {
+      const noVerticalWin = [
+        ['x', 'o', 'x'],
+        ['o', 'o', 'o'],
+        ['x', 'x', 'o'],
+      ];
+
+      expect(validation.isVerticalWin(noVerticalWin)).to.equal(false);
+    });
+
+    describe('should return true if a vertical win is present in', function() {
+      it('the first col', function() {
+        const firstCol = [
+          ['x', 'x', 'x'],
+          ['x', 'o', 'o'],
+          ['x', 'x', 'o'],
+        ];
+
+        expect(validation.isVerticalWin(firstCol)).to.equal(true);
+      });
+
+      it('the second col', function() {
+        const secondCol = [
+          ['x', 'x', 'x'],
+          ['o', 'x', 'o'],
+          ['x', 'x', 'o'],
+        ];
+
+        expect(validation.isVerticalWin(secondCol)).to.equal(true);
+      });
+
+      it('the third col', function() {
+        const thirdCol = [
+          ['x', 'o', 'o'],
+          ['x', 'x', 'o'],
+          ['o', 'o', 'o'],
+        ];
+
+        expect(validation.isVerticalWin(thirdCol)).to.equal(true);
+      });
+
+      it('any col', function() {
+        const anyCol = [
+          ['x', 'o', 'x', 'o', 'o', 'x'],
+          ['o', 'o', 'o', 'o', 'x', 'x'],
+          ['x', 'x', 'o', 'o', 'o', 'o'],
+          ['x', 'x', 'o', 'o', '', ''],
+          ['x', 'x', 'o', 'o', '', ''],
+          ['x', 'x', 'x', 'o', 'x', 'x'],
+        ];
+
+        expect(validation.isVerticalWin(anyCol)).to.equal(true);
       });
     });
   });
